6865da2e2fa89c8ca2cee1bf6730632e3b9e0e2f
krishnadev7 <krishnadevv12@gmail.com>
2024-09-13 11:02:31 +0530

    completed thank-you page

--- src/app/thank-you/Thankyou.tsx
+++ src/app/thank-you/Thankyou.tsx
@@ -6,6 +6,8 @@ import { useSearchParams } from "next/navigation";
 import { Loader2 } from "lucide-react";
 import Confetti from "react-dom-confetti";
 import { useEffect, useState } from "react";
+import PhonePreview from "@/components/PhonePreview";
+import { formatPrice } from "@/lib/utils";
 
 const Thankyou = () => {
     const [showConfetti, setShowConfetti] = useState(false);
@@ -50,6 +52,7 @@ const Thankyou = () => {
     const { configuration, billingAddress, shippingAddress, amount } = data;
     const { color } = configuration;
 
+
     return (
         <>        <div aria-hidden="true" className="pointer-events-none select-none absolute 
                 inset-0 overflow-hidden flex justify-center">
@@ -73,8 +76,63 @@ const Thankyou = () => {
 
                     <div className="flex space-x-6 overflow-hidden mt-4 rounded-xl bg-gray-900/5 ring-1 ring-inset
                      ring-gray-900/10 lg:rounded-2xl">
-                        
-                     </div>
+                        <PhonePreview croppedImageUrl={configuration.croppedImageUrl!} color={configuration.color!} />
+                    </div>
+
+                    <div>
+                        <div className="grid grid-cols-2 gap-x-6 py-10 text-sm">
+                            <div>
+                                <p className="font-medium text-gray-900">Shipping Address</p>
+                                <div className="mt-2 text-zinc-700">
+                                    <address className="not-italic">
+                                        <span className="block">{billingAddress?.name}</span>
+                                        <span className="block">{billingAddress?.street}</span>
+                                        <span className="block">{billingAddress?.postalCode} {billingAddress?.city}</span>
+                                    </address>
+                                </div>
+                            </div>
+                            <div>
+                                <p className="font-medium text-gray-900">Billing Address</p>
+                                <div className="mt-2 text-zinc-700">
+                                    <address className="not-italic">
+                                        <span className="block">{billingAddress?.name}</span>
+                                        <span className="block">{billingAddress?.street}</span>
+                                        <span className="block">{billingAddress?.postalCode} {billingAddress?.city}</span>
+                                    </address>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='grid grid-cols-2 gap-x-6 border-t border-zinc-200 py-10 text-sm'>
+                            <div>
+                                <p className='font-medium text-zinc-900'>Payment status</p>
+                                <p className='mt-2 text-zinc-700'>Paid</p>
+                            </div>
+
+                            <div>
+                                <p className='font-medium text-zinc-900'>Shipping Method</p>
+                                <p className='mt-2 text-zinc-700'>
+                                    Delhivery takes up to 3 working days
+                                </p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div className='space-y-6 border-t border-zinc-200 pt-10 text-sm'>
+                        <div className='flex justify-between'>
+                            <p className='font-medium text-zinc-900'>Subtotal</p>
+                            <p className='text-zinc-700'>{formatPrice(amount)}</p>
+                        </div>
+                        <div className='flex justify-between'>
+                            <p className='font-medium text-zinc-900'>Shipping</p>
+                            <p className='text-zinc-700'>{formatPrice(0)}</p>
+                        </div>
+                        <div className='flex justify-between'>
+                            <p className='font-medium text-zinc-900'>Total</p>
+                            <p className='text-zinc-700'>{formatPrice(amount)}</p>
+                        </div>
+                    </div>
 
                 </div>
             </div>
